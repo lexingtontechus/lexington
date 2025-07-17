899cbcdb14cac4aa1bb94363fde3e415dbbe5f3f
Aloysius Teo <aloysiusteo72@gmail.com>
2025-07-17 06:08:12 +0000

    Updates

--- app/not-found.js
+++ app/not-found.js
@@ -1,14 +1,13 @@
 "use client"; // Error components must be Client Components
 
 import Link from "next/link";
-import { Player } from "@lottiefiles/react-lottie-player";
 import LogoLXT from "./components/logo";
 export default function NotFound() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <LogoLXT size="64" padding="0" className="mx-auto" />
+          <LogoLXT size="64" padding="2" className="mx-auto" />
           <h1 className="m-4 text-5xl font-bold uppercase">
             WEB3 NODE STATION
           </h1>
